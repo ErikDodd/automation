db9eb5d98fb56dd44b1900872d6b7c8962983300
Erik M. Dodd <emdodd27@gmail.com>
2023-01-12 18:30:42 -0800

    finished lab for now. Submitting shortly. Still need to format the phone numbers

--- README.md
+++ README.md
@@ -1,19 +1,37 @@
-LAB - Class xx
-Project: Project Name Here
-Author: Student/Group Name
-Links and Resources
-back-end server url (when applicable)
-front-end application (when applicable)
-Setup
+# LAB - Class 19
+## Project: Automation
+## Author: Erik Dodd
+
+### Links and Resources
+back-end server url (when applicable) **N/A**
+front-end application (when applicable) **N/A**
+
+### Setup
 .env requirements (where applicable)
 i.e.
 
-PORT - Port Number
-DATABASE_URL - URL to the running Postgres instance/db
+PORT - Port Number **N/A**
+DATABASE_URL - URL to the running Postgres instance/db **N/A**
+
+
 How to initialize/run your application (where applicable)
 e.g. python main.py
-How to use your library (where applicable)
+
+- python automation.py
+
+
+How to use your library (where applicable) **N/A**
+
 Tests
-How do you run tests?
-Any tests of note?
+
+How do you run tests? **N/A**
+
+Any tests of note? **N/A**
+
 Describe any tests that you did not complete, skipped, etc
+
+## Attributions
+
+A special thank you to ChatGPT. This helped me with getting the regex for email and 
+phone addresses. A super helpful tool for making regex a lot faster and a lot
+less frustrating.
--- automation.py
+++ automation.py
@@ -18,14 +18,22 @@ with open("./potential_contacts.txt", "r") as f:
     # Remove duplicates and prints the list of numbers
     phone_without_duplicates = list(set(phone_numbers))
 
+    # Sort the phone numbers in Ascending Order
+    sorted_phone_numbers = phone_without_duplicates.sort()
+
     # Check new total amount of phone numbers without duplicates = 100
     total_phone_without_duplicates = len(phone_without_duplicates)
+
 
     # Regex Email pattern. A special thank you/attribute to ChatGPT for providing the basis for this line of code
     email_regex_pattern = r'[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}'
 
     # Find all the email addresses in potential_contacts
     email_addresses = re.findall(email_regex_pattern, text_from_potential_contacts)
+
+    email_without_duplicates = list(set(email_addresses))
+
+    total_email_without_duplicates = len(email_without_duplicates)
 
     # Check the total amount of email addresses = 115
     total_number_of_email = len(email_addresses)
@@ -35,16 +43,17 @@ with open("phone_numbers.txt", "w") as f:
         f.write(phone + '\n')
 
 with open('emails.txt', 'w') as f:
-    for email in email_addresses:
+    for email in email_without_duplicates:
         f.write(email + '\n')
 
 
 
     # Print Statements for checking phone numbers and emails
     # print(phone_numbers)
-    print(f"Total phone numbers with duplicates: {total_phone_numbers}")
-    print(f"Total phone numbers without duplicates: {total_phone_without_duplicates}")
-    # print(phone_without_duplicates)
-    # print(email_addresses)
-    # print(total_number_of_email)
+    #print(f"Total phone numbers with duplicates: {total_phone_numbers}")
+    #print(f"Total phone numbers without duplicates: {total_phone_without_duplicates}")
+    #print(phone_without_duplicates)
+    # print(f"email_addresses)
+    #print(f"Total number of email with duplicates: {total_number_of_email}")
+    #print(f"Total number of email without duplicates: {total_email_without_duplicates}")
 
